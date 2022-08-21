706a58300a9b1429fe1032a4ca59e70971a703de
scoltzero <scoltzero@qq.com>
2022-08-21 23:52:54 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -10,8 +10,8 @@
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
 #
 
- Uncomment a feed source
- sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+#Uncomment a feed source
+#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
